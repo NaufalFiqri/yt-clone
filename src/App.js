f5006b2e0d88fff63ff7523ab3dec1f9934a1658
NaufalFiqri <naufal11bale@gmail.com>
2023-06-09 12:03:48 +0800

    init constants file in utils

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+import { Box } from "@mui/material";
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/video/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
